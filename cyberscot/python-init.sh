a0989980dea7f6238fa1127555184e5ca665b89b
Craig Thacker <craigthackerx@gmail.com>
2021-11-24 00:03:41 +0000

    update app

---
+++ cyberscot/python-init.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -xe
+VIRTUAL_ENV="app"
+CURRENT_LOCATION="$(pwd)"
+
+#Forces script to execute current directory
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+
+cd "${SCRIPT_DIR}" && \
+    virtualenv "${VIRTUAL_ENV}" && \
+    chmod +x "${SCRIPT_DIR}"/"${VIRTUAL_ENV}"/bin/activate && \
+    source "${SCRIPT_DIR}"/"${VIRTUAL_ENV}"/bin/activate && \
+    pip3 install fastapi uvicorn
+    cd "${CURRENT_LOCATION}" && \
+    pip3 freeze > requirements.txt
