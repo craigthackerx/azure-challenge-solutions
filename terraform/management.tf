1eab5581016d104353f4c6c363865662c3b0bcd9
Craig Thacker <craigthackerx@gmail.com>
2021-12-31 19:26:32 +0000

    fetch secret

--- terraform/management.tf
+++ terraform/management.tf
@@ -11,3 +11,9 @@ data "azurerm_key_vault" "mgmt_kv" {
   name                = "kv-${var.AZURE_SHORT}-${var.loc}-${terraform.workspace}-mgt-01"
   resource_group_name = data.azurerm_resource_group.mgmt_rg.name
 }
+
+data "azurerm_key_vault_secret" "mgmt_local_admin_pwd" {
+  key_vault_id = data.azurerm_key_vault.mgmt_kv.id
+  name         = "Local${var.AZURE_SHORT}Admin${terraform.workspace}-pwd"
+
+}
