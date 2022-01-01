cca2d1aca44b8a34e5ce15b73a4b45e116927512
Craig Thacker <craigthackerx@gmail.com>
2022-01-01 02:32:16 +0000

    update name

--- azure-init/scripts/WindowsInitScript.ps1
+++ azure-init/scripts/WindowsInitScript.ps1
@@ -7,4 +7,4 @@ Set-ExecutionPolicy RemoteSigned -scope CurrentUser ; `
 Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh') ;  `
 
 #Install Chocolatey
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1')) ; `
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
