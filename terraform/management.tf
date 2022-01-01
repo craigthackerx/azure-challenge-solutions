f1d331c8706a0fe32139fc3e9790953384b3bd01
Craig Thacker <craigthackerx@gmail.com>
2022-01-01 03:29:01 +0000

    fix typo

--- terraform/management.tf
+++ terraform/management.tf
@@ -1,14 +1,18 @@
+variable "mgmt_loc" {
+  default = "uks"
+}
+
 data "azurerm_resource_group" "mgmt_rg" {
-  name = "rg-${var.short}-${var.loc}-${terraform.workspace}-mgt"
+  name = "rg-${var.short}-${var.mgmt_loc}-${terraform.workspace}-mgt"
 }
 
 data "azurerm_ssh_public_key" "mgmt_ssh_key" {
-  name                = "ssh-${var.short}-${var.loc}-${terraform.workspace}-pub-mgt"
+  name                = "ssh-${var.mgmt_loc}-${var.loc}-${terraform.workspace}-pub-mgt"
   resource_group_name = data.azurerm_resource_group.mgmt_rg.name
 }
 
 data "azurerm_key_vault" "mgmt_kv" {
-  name                = "kv-${var.short}-${var.loc}-${terraform.workspace}-mgt-01"
+  name                = "kv-${var.short}-${var.mgmt_loc}-${terraform.workspace}-mgt-01"
   resource_group_name = data.azurerm_resource_group.mgmt_rg.name
 }
 
