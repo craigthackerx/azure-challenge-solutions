3f4698d6b0fe67346d98543361d77482d30bdead
Craig Thacker <craigthackerx@gmail.com>
2022-01-03 20:18:52 +0000

    remove name filter

--- terraform/lb.tf
+++ terraform/lb.tf
@@ -103,7 +103,7 @@ resource "azurerm_lb_probe" "lb_grafana_probe" {
 #}
 
 output "index" {
-  value = azurerm_network_interface.lnx_nic[*].ip_configuration.name
+  value = azurerm_network_interface.lnx_nic[*].ip_configuration
 }
 
 resource "azurerm_network_security_rule" "AlllowLBInbound" {
