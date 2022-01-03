e23df4de5d5fce6b082bba3bc0750e371626b9e8
Craig Thacker <craigthackerx@gmail.com>
2022-01-03 20:07:19 +0000

    update output

--- terraform/lb.tf
+++ terraform/lb.tf
@@ -103,7 +103,7 @@ resource "azurerm_lb_probe" "lb_grafana_probe" {
 #}
 
 output "index" {
-  value = azurerm_network_interface.lnx_nic.*.ip_configuration
+  value = azurerm_network_interface.lnx_nic.*.ip_configuration.name
 }
 
 resource "azurerm_network_security_rule" "AlllowLBInbound" {
