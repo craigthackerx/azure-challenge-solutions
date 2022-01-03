9ebd1a9b5ee3f2c826c475bb7516ed6c05682158
Craig Thacker <craigthackerx@gmail.com>
2022-01-03 07:45:31 +0000

    update to add script

---
+++ azure-init/scripts/linux-init.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+[ "$(whoami)" = root ] || { sudo "$0" "$@"; exit $?; }
+
+set -xeuo pipefail
+
+timedatectl set-timezone Europe/London && \
+setsebool -P container_manage_cgroup on && \
+firewall-cmd --zone=public --add-port=8080/tcp && \
+firewall-cmd --zone=public --add-port=8090/tcp && \
+firewall-cmd --reload && \
+loginctl enable-linger 1000 && \
+
+yum update -y && \
+yum install -y \
+  podman \
+  python3-pip \
+  git \
+  yum-utils \
+  dnf-plugins-core \
+  curl \
+  zip \
+  unzip \
+  openssl && \
+
+yum clean all
+
+
+   if [ "$(command -v pip)" ]; then
+
+     export PATH=$PATH:${HOME}/.local/bin && \
+
+     git clone https://github.com/craigthackerx/azure-challenge-solutions.git && \
+
+     pip3 install --user podman-compose && \
+
+     cd azure-challenge-solutions/container/podman/grafana && \
+     mkdir -p grafana-data && \
+     systemctl --user enable --now dbus.socket && \
+     echo "The VM is now setup." && \
+
+     cd .. && podman-compose up -d
+
+   else
+     echo "Error running user script"
+   fi
